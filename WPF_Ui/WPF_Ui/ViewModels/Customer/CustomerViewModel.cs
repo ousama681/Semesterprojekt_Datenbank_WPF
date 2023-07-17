67a9c5ca22a4f01b980180e94a511ef7ed375d0c
Kim Devaux <kimdevaux.gmx.ch@zbw-online.ch>
2023-07-17 16:34:14 +0200

    added search function to customers

--- WPF_Ui/WPF_Ui/ViewModels/Customer/CustomerViewModel.cs
+++ WPF_Ui/WPF_Ui/ViewModels/Customer/CustomerViewModel.cs
@@ -10,6 +10,8 @@ using WPF_Ui.Views.Pages.Customer;
 using WPF_Ui.Views.Windows;
 using WPF_Ui.Models;
 using RelayCommand = CommunityToolkit.Mvvm.Input.RelayCommand;
+using System.Linq;
+using System;
 
 namespace WPF_Ui.ViewModels.Customer
 {
@@ -21,12 +23,16 @@ namespace WPF_Ui.ViewModels.Customer
         public ICommand AddCommand { get; set; }
         public readonly ICustomerRepository _customerRepository;
         public readonly ITownRepository _townRepository;
+        MainWindow? mainWindow;
 
         private ObservableCollection<WPF_Ui.Models.Customer> _customerList;
         public ObservableCollection<WPF_Ui.Models.Customer> CustomerList
         {
             get { return _customerList; }
-            set { SetProperty(ref _customerList, value); }
+            set 
+            {   SetProperty(ref _customerList, value);
+                FilterCustomers();
+            }
         }
         private WPF_Ui.Models.Customer _selectedCustomer;
         public WPF_Ui.Models.Customer SelectedCustomer
@@ -35,7 +41,25 @@ namespace WPF_Ui.ViewModels.Customer
             set { SetProperty(ref _selectedCustomer, value); }
         }
 
-        MainWindow? mainWindow;
+        private string _searchText;
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                SetProperty(ref _searchText, value);
+                FilterCustomers();
+            }
+        }
+
+        private ObservableCollection<WPF_Ui.Models.Customer> _filteredCustomers;
+        public ObservableCollection<WPF_Ui.Models.Customer> FilteredCustomers
+        {
+            get { return _filteredCustomers; }
+            set { SetProperty(ref _filteredCustomers, value); }
+        }
+
+
 
         public CustomerViewModel(ICustomerRepository customerRepository, ITownRepository townRepository)
         {
@@ -55,6 +79,7 @@ namespace WPF_Ui.ViewModels.Customer
             }
             List<WPF_Ui.Models.Customer> customers = await _customerRepository.GetAllAsync();
             CustomerList = new ObservableCollection<WPF_Ui.Models.Customer>(customers);
+            SearchText = string.Empty;
         }
 
         public void OnNavigatedFrom()
@@ -66,6 +91,26 @@ namespace WPF_Ui.ViewModels.Customer
         {
             _isInitialized = true;
            
+        }
+
+        private void FilterCustomers()
+        {
+            if (string.IsNullOrWhiteSpace(SearchText))
+            {
+                FilteredCustomers = CustomerList;
+            }
+            else
+            {
+                FilteredCustomers = new ObservableCollection<WPF_Ui.Models.Customer>(
+                CustomerList.Where(c =>
+                c.Name.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase) ||
+                c.Nr.ToString().Contains(SearchText, StringComparison.InvariantCultureIgnoreCase) ||
+                c.Email.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase) ||
+                c.Street.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase) ||
+                c.Website.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase) ||
+                c.Town.City.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase) ||
+                c.Town.ZipCode.Contains(SearchText, StringComparison.InvariantCultureIgnoreCase)));
+            }
         }
 
         public void OnAdd()
