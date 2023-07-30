17da274f9e85789c73cf8bb49352118d4fa1c00e
Kim Devaux <kimdevaux@gmx.ch>
2023-07-30 17:13:32 +0200

    change parentId of articlegroup in migration to int

--- WPF_Ui/WPF_Ui/Migrations/20221225152433_AddedModelsAndEntityConfigs.cs
+++ WPF_Ui/WPF_Ui/Migrations/20221225152433_AddedModelsAndEntityConfigs.cs
@@ -18,7 +18,7 @@ namespace WPF_Ui.Migrations
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    ParentId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                    ParentId = table.Column<int>(type: "int", nullable: true)
                 },
                 constraints: table =>
                 {
